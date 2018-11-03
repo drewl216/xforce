06a9817815903f5dff156bef4fb9ffb4b077ba42
Jacob Larkin <jkub66@gmail.com>
2018-11-03 15:10:59 -0600

    bullet modification

--- js/bullet.js
+++ js/bullet.js
@@ -3,16 +3,15 @@ class Bullet extends PhysicsObj
   constructor(){
     super();
     this.diam = 3;
+    this.color = {r:255, g:0, b:0};
   }
 
   draw(){
-    if(this.type=='bullet') {
 			context.beginPath();
 			context.arc(this.x, this.y, this.diam/2, 0, 2 * Math.PI, false);
 	    context.fillStyle = 'rgb('+Math.round(this.color.r)+','+Math.round(this.color.g)+','+Math.round(this.color.b)+')';//'#000000';   -   The object color
 	    context.fill();
 	    context.strokeStyle = 'rgb('+Math.round(this.color.r)+','+Math.round(this.color.g)+','+Math.round(this.color.b)+')';//'#000000';  - The line arround the object
 	    context.stroke();
-    }
   }
 }
