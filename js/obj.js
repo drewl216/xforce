19916fd3d3934b1db0ed0371b246bf2f9d39e903
Sam Larkin <ssamm1545@gmail.com>
2018-11-03 14:57:26 -0600

    factor main.js to util.js added serialization to obj

--- js/obj.js
+++ js/obj.js
@@ -15,8 +15,8 @@ class xobj
 	colorops[1]={r:0, g:150, b:0};
 	colorops[2]={r:0, g:0, b:255};
 
-	this.x = myrand(0,xmax*2/3)+xmax/6;
-	this.y = myrand(0,ymax*2/3)+ymax/6;
+	this.x = Util.rand(0,xmax*2/3)+xmax/6;
+	this.y = Util.rand(0,ymax*2/3)+ymax/6;
 	this.forces = Victor(0,0);
 	this.vel = Victor(0,0);
 	this.rot = Victor(0,-1);
@@ -273,6 +273,37 @@ class xobj
 			console.log(new_o2velN);*/
 
 		}
+
+
 	}; //end collision function
 
+	toJson() 
+	{
+		return JSON.stringify(this);
+	}
+
+
+	/**
+		create from a json version
+		@param string json_str a json encoded xobj
+	*/
+	static fromJson(json_str)
+	{
+		var object_parsed = JSON.parse(json_str);
+		var obj = new xobj();
+		for (var prop in object_parsed) {
+			if (obj.hasOwnProperty(prop)) {
+			obj[prop] = object_parsed[prop];}
+		}
+		return obj;
+	}
+
+
 } //end xobj class
+
+/**
+	allow the code to be loaded in either the browser or nodejs
+*/
+typeof module=== 'undefined'? '': (function(exports){
+   module.exports=xobj;
+});
