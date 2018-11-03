16b7298f7f23a398cf7b1ff24dac21c08e8e5208
Jacob Larkin <jkub66@gmail.com>
2018-11-03 14:15:44 -0600

    added asteroid

---
+++ js/asteroid.js
@@ -0,0 +1,18 @@
+class asteroid extends xobj
+{
+  constructor(){
+    super();
+  }
+
+  draw(){
+    context.beginPath();
+    context.arc(this.x, this.y, this.diam/2, 0, 2 * Math.PI, false);
+    //context.shadowColor = 'rgb('+Math.round(this.color.r)+','+Math.round(this.color.g)+','+Math.round(this.color.b)+')';//'#000000';   - The object shadow
+    context.shadowBlur = this.diam*2;
+    context.fillStyle = 'rgb('+Math.round(this.color.r)+','+Math.round(this.color.g)+','+Math.round(this.color.b)+')';//'#000000';   -   The object color
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = 'rgb('+Math.round(this.color.r)+','+Math.round(this.color.g)+','+Math.round(this.color.b)+')';//'#000000';  - The line arround the object
+    context.stroke();
+  }
+}
--- js/obj.js
+++ js/obj.js
@@ -40,17 +40,6 @@ class xobj
 
 
 	draw() {
-    if(this.type=='planet' || this.type=='nucleus') {
-			context.beginPath();
-			context.arc(this.x, this.y, this.diam/2, 0, 2 * Math.PI, false);
-	    //context.shadowColor = 'rgb('+Math.round(this.color.r)+','+Math.round(this.color.g)+','+Math.round(this.color.b)+')';//'#000000';   - The object shadow
-			context.shadowBlur = this.diam*2;
-	    context.fillStyle = 'rgb('+Math.round(this.color.r)+','+Math.round(this.color.g)+','+Math.round(this.color.b)+')';//'#000000';   -   The object color
-	    context.fill();
-	    context.lineWidth = 1;
-	    context.strokeStyle = 'rgb('+Math.round(this.color.r)+','+Math.round(this.color.g)+','+Math.round(this.color.b)+')';//'#000000';  - The line arround the object
-	    context.stroke();
-	  }
     if(this.type=='bullet') {
 			context.beginPath();
 			context.arc(this.x, this.y, this.diam/2, 0, 2 * Math.PI, false);
