bc8858b5f47ff4be6784efa67561140bdf154872
Jacob Larkin <jkub66@gmail.com>
2018-11-03 20:17:14 -0600

    fixed world

--- js/world.js
+++ js/world.js
@@ -9,7 +9,7 @@ class World
 	this.win_condition = "kills"; //(kills, elimination, points, capture, base_destroy)
 	this.win_limit = 5;
 	this.respawn_timer = 5000; //ms
-	// this.players = 
+	// this.players =
 	//this.teams=
 
   }
@@ -18,23 +18,23 @@ class World
     //ADD ALL OTHER OBJECTS
     for(var n=0; n<num_rand_planets; n++) {
     	var obj = new Asteroid();
-    	obj.positionRandomly(xmax,ymax);
-    	obj.mass=100;
+    	obj.positionRandomly(-level_edge.x,-level_edge.y,level_edge.x,level_edge.y);
     	obj.index = objarr.push(obj) - 1;
     }
-	for (var n=0; n<this.player_qty; n++) {
-		var obj = new Ship();
-		obj.type='player';
-		obj.immobile=false;
-		obj.positionRandomly(-level_edge.x,-level_edge.y,level_edge.x,level_edge.y);
-		obj.mass = 100;
-		obj.density = 0;
-		obj.color = {r:0, g:100, b:200};
-		obj.diam = 20;
-		obj.index = objarr.push(obj) - 1;
-		player1_obj = objarr[obj.index];
-		players.push(obj.index);
-	}
+
+    var obj = new Ship();
+    obj.type='player';
+    obj.immobile=false;
+    obj.positionRandomly(-level_edge.x,-level_edge.y,level_edge.x,level_edge.y);
+    obj.mass = 100;
+    obj.density = 0;
+    obj.color = {r:0, g:100, b:200};
+    obj.diam = 20;
+    obj.index = objarr.push(obj) - 1;
+    player1_obj = objarr[obj.index];
+    players.push(obj.index);
+
+
   }
 
   start_sim() {
@@ -43,7 +43,7 @@ class World
   }
 
   run_sim_step_loop() {
-  	step_cnt++;
+    step_cnt++;
   	full_cnt++;
   	setTimeout(function(parent_this){
   		if(game_state=='play') {
@@ -59,43 +59,65 @@ class World
   }
 
   sim_step() {
-  	//APPLY MOVEMENT FORCES TO PLAYERS
+    //APPLY MOVEMENT FORCES TO PLAYERS
   	for(var i=0; i<players.length; i++) {
   		var oid = players[i];
   		var pobj = objarr[oid];
+  		if(pobj.control_scheme=="directional"){
+  			if(keystate[LEFTKEY]) pobj.vel.x -= FORWARD_ACCEL*step_size;
+  			if(keystate[RIGHTKEY]) pobj.vel.x += FORWARD_ACCEL*step_size;
+  			if(keystate[UPKEY]) { pobj.vel.y -= FORWARD_ACCEL*step_size; }
+  			if(keystate[DOWNKEY]) { pobj.vel.y += FORWARD_ACCEL*step_size; }
+  			if(keystate[FIREKEY]) {
 
-  		if(keystate[LEFTKEY]) pobj.rot.rotate(-1*ROTATE_SPEED*step_size);
-  		if(keystate[RIGHTKEY]) pobj.rot.rotate(1*ROTATE_SPEED*step_size);
-  		if(keystate[UPKEY]) { pobj.vel.x += pobj.rot.x*FORWARD_ACCEL*step_size; pobj.vel.y += pobj.rot.y*FORWARD_ACCEL*step_size; }
-  		if(keystate[DOWNKEY]) { pobj.vel.x += pobj.rot.x*-1*FORWARD_ACCEL*step_size; pobj.vel.y += pobj.rot.y*-1*FORWARD_ACCEL*step_size; }
-  		if(keystate[FIREKEY]) {
-
-  			if(!pobj.last_fire_time || pobj.last_fire_time<(Date.now()-RATE_OF_FIRE_MS)) {
-  				pobj.last_fire_time = Date.now();
-  				var obj = new Bullet();
-  				obj.x = pobj.x + pobj.rot.x*(pobj.diam+10);
-  				obj.y = pobj.y + pobj.rot.y*(pobj.diam+10);
-  				obj.vel.x = pobj.vel.x + pobj.rot.x*BULLET_SPEED;
-  				obj.vel.y = pobj.vel.y + pobj.rot.y*BULLET_SPEED;
-  				objarr.push(obj);
-
-  				//accelerate backwards
-  				pobj.vel.x -= pobj.rot.x * BAKUP_SPEED;
-  				pobj.vel.y -= pobj.rot.y * BAKUP_SPEED;
+  				if(!pobj.last_fire_time || pobj.last_fire_time<(Date.now()-RATE_OF_FIRE_MS)) {
+  					pobj.last_fire_time = Date.now();
+  					var obj = new Projectile();
+  					var vect = new Victor(pobj.vel.x, pobj.vel.y);
+  					vect.norm();
+  					obj.x = pobj.x + obj.diam*vect.x;
+  					obj.y = pobj.y+ obj.diam*vect.y;
+  					obj.vel.x = pobj.vel.x + BULLET_SPEED*vect.x;
+  					obj.vel.y = pobj.vel.y + BULLET_SPEED*vect.y;
+  					objarr.push(obj);
+  					snd_bloip.play();
+  				}
   			}
   		}
+  		else {
+  			if(keystate[LEFTKEY]) pobj.rot.rotate(-1*ROTATE_SPEED*step_size);
+  			if(keystate[RIGHTKEY]) pobj.rot.rotate(1*ROTATE_SPEED*step_size);
+  			if(keystate[UPKEY]) { pobj.vel.x += pobj.rot.x*FORWARD_ACCEL*step_size; pobj.vel.y += pobj.rot.y*FORWARD_ACCEL*step_size; }
+  			if(keystate[DOWNKEY]) { pobj.vel.x += pobj.rot.x*-1*FORWARD_ACCEL*step_size; pobj.vel.y += pobj.rot.y*-1*FORWARD_ACCEL*step_size; }
+  			if(keystate[FIREKEY]) {
 
+  				if(!pobj.last_fire_time || pobj.last_fire_time<(Date.now()-RATE_OF_FIRE_MS)) {
+  					pobj.last_fire_time = Date.now();
+  				  var obj = new Projectile();
+  					obj.x = pobj.x + pobj.rot.x*(pobj.diam+10);
+  					obj.y = pobj.y + pobj.rot.y*(pobj.diam+10);
+  					obj.vel.x = pobj.vel.x + pobj.rot.x*BULLET_SPEED;
+  					obj.vel.y = pobj.vel.y + pobj.rot.y*BULLET_SPEED;
+  					objarr.push(obj);
+  					snd_bloip.play();
+
+  					//accelerate backwards
+  					pobj.vel.x -= pobj.rot.x * BAKUP_SPEED;
+  					pobj.vel.y -= pobj.rot.y * BAKUP_SPEED;
+  				}
+  			}
+  		}
   	}
 
 
   	//APPLY FORCES TO RECALCULATE VELOCITY VECTORS
 
   	//PERFORM COLLISSIONS AND CLEANUP
-    for(var i=0; i<objarr.length; i++)
+  	for(var i=0; i<objarr.length; i++)
   	{
   		var p1 = objarr[i];
   		if(!p1 || p1.disabled) continue; //this object no longer exists
-  		if(p1.type=='bullet' && (Math.abs(p1.vel.x)+Math.abs(p1.vel.y)) < BULLET_SPEED*.8) { delete(objarr[i]); continue; }
+  //		if(p1.type=='bullet' && (Math.abs(p1.vel.x)+Math.abs(p1.vel.y)) < BULLET_SPEED*.8) { delete(objarr[i]); continue; }
 
   		for(var j=i+1; j<objarr.length; j++)
   		{
@@ -104,12 +126,12 @@ class World
   			if(p1.type=='bullet' && p2.type=='bullet') continue; //they're both bullets
 
   			//remove old/slow bullets (they also get removed if they leave the screen)
-  			if(p2.type=='bullet' && (Math.abs(p2.vel.x)+Math.abs(p2.vel.y)) < BULLET_SPEED*.8) { delete(objarr[j]); continue; }
+  //		if(p2.type=='bullet' && (Math.abs(p2.vel.x)+Math.abs(p2.vel.y)) < BULLET_SPEED*.8) { delete(objarr[j]); continue; }
 
-  			var dist = Math.sqrt( Math.pow((p1.x-p2.x), 2) + Math.pow((p1.y-p2.y), 2) );
-  			var min_dist = p1.diam/2 + p2.diam/2; //objects can't possibly be closer than this
-  			var use_dist = dist<min_dist? min_dist:dist;
-  			var is_touching = ((dist - (p1.diam/2) - (p2.diam/2)) <= 0);
+  			dist = Math.sqrt( Math.pow((p1.x-p2.x), 2) + Math.pow((p1.y-p2.y), 2) );
+  			min_dist = p1.diam/2 + p2.diam/2; //objects can't possibly be closer than this
+  			use_dist = dist<min_dist? min_dist:dist;
+  			is_touching = ((dist - (p1.diam/2) - (p2.diam/2)) <= 0);
 
   			//PERFORM COLLISION
   			if(!p1.no_collision && !p2.no_collision) {
@@ -120,7 +142,7 @@ class World
   		}
   	}
 
-    //APPLY GRAVITY & OTHHER FORCES
+  	//APPLY GRAVITY & OTHHER FORCES
   	for(var i=0; i<objarr.length; i++)
   	{
   		var p1 = objarr[i];
@@ -145,14 +167,13 @@ class World
   			if(p1.no_gravity || p2.no_gravity) grav_force=0;
   			else { grav_force = grav_const*p2.mass*p1.mass / Math.pow(use_dist,2); }
 
-        var total_force = 0;
   			if(!p1.immobile) {
-  				total_force = (p1.no_gravity_movement? 0:grav_force);
+  				var total_force = (p1.no_gravity_movement? 0:grav_force);
   				p1.vel.x += (uvect_i*total_force) / p1.mass * step_size;
   				p1.vel.y += (uvect_j*total_force) / p1.mass * step_size;
   			}
   			if(!p2.immobile) {
-  				total_force = (p2.no_gravity_movement? 0:grav_force);
+  				var total_force = (p2.no_gravity_movement? 0:grav_force);
   				//if(Math.abs(total_force) > .00001) {
   				p2.vel.x -= (uvect_i*total_force) / p2.mass * step_size;
   				p2.vel.y -= (uvect_j*total_force) / p2.mass * step_size;
@@ -186,15 +207,14 @@ class World
   }
 
   sim_draw() {
-  	//scroll to new player position
+    //scroll to new player position
   	//var wrapper = document.getElementById('canvasWrapper');
-  	screen_pos_x = player1_obj.x - Math.round(xmax/2);
-  	screen_pos_y = player1_obj.y - Math.round(ymax/2);
+  	var screen_pos_x = player1_obj.x - Math.round(xmax/2);
+  	var screen_pos_y = player1_obj.y - Math.round(ymax/2);
+  	var screen_max_x = screen_pos_x + xmax;
+  	var screen_max_y = screen_pos_y + ymax;
   	//wrapper.scrollTop = player1_obj.x - Math.round(xmax/2);
   	//wrapper.scrollLeft = player1_obj.y - Math.round(ymax/2);
-
-  	//save the default transformation matrix
-  	context.save(); //push()
 
   	//CLEAR OLD
   	if(clear_each_frame) context.clearRect(0, 0, canvas.width, canvas.height);
@@ -204,7 +224,21 @@ class World
   		context.shadowColor = '#FFFFFF';
   		context.fillRect(0, 0, canvas.width, canvas.height);
   		context.stroke();
-  	}
+  	}	//save the default transformation matrix
+
+
+  	//DRAW BACKGROUND
+  	context.save(); //push()
+  	//TRANSLATE BY USER POSIITON
+  	var level_width_plus_half_screen = 2*level_edge.x-(.5*canvas.width);
+  	var level_height_plus_half_screen = 2*level_edge.y-(.5*canvas.height);
+  	//percent_x =
+  	context.translate(-screen_pos_x, -screen_pos_y);
+  	context.drawImage(background,-level_edge.x-(.5*canvas.width),-level_edge.y-(.5*canvas.height), level_edge.x*2+canvas.width,level_edge.y*2+canvas.height);
+  	context.restore(); //pop()
+
+
+  	context.save(); //push()
 
   	//TRANSLATE BY USER POSIITON
   	context.translate(-screen_pos_x, -screen_pos_y);
@@ -214,8 +248,13 @@ class World
   	context.beginPath();
   	context.lineWidth="6";
   	context.rect(-level_edge.x, -level_edge.y, level_edge.x*2, level_edge.y*2);
-    context.strokeStyle = 'rgb('+100+','+100+','+100+')';//'#000000';  - The line arround the object
+   context.strokeStyle = 'rgb('+100+','+100+','+100+')';//'#000000';  - The line arround the object
     context.stroke();
+    //reset line width to 1
+context.lineWidth="1";
+
+
+
 
 
   	//DRAW EACH OBJECT
@@ -223,6 +262,11 @@ class World
   		//DRAW EACH POINT
   		var obj = objarr[i];
   		if(!obj || obj.disabled) continue; //this point no longer exists
+  		var objright = obj.x+obj.diam/2;
+  		var objleft = obj.x-obj.diam/2;
+  		var objtop = obj.y-obj.diam/2;
+  		var objbot = obj.y+obj.diam/2;
+  		if(objright<screen_pos_x || objleft > screen_max_x || objtop>screen_max_y || objbot < screen_pos_y) continue; //if the objects are outside the visible screen don't draw them.
   		obj.draw();
   	}
 
